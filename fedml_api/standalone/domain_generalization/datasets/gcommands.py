44f4db7ce170802e80289e70a539cf153d199691
Alejandro Nardo González (private) <aleja@alejandros-macbook-pro-1.home>
2025-05-15 21:52:31 +0200

    fixing cuda loss function not implemented for short

--- fedml_api/standalone/domain_generalization/datasets/gcommands.py
+++ fedml_api/standalone/domain_generalization/datasets/gcommands.py
@@ -112,7 +112,7 @@ class GCommandsDataset(SPEECHCOMMANDS):
         if self.data is not None:
             return (
                 self.data[index].to(torch.float32),
-                self.targets[index].to(torch.int16),
+                self.targets[index].to(torch.int32),
             )
 
         waveform, sample_rate, label, speaker_id, utterance_number = (
